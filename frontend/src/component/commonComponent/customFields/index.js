d55785602e53592ef4a6f9bae3a6caa311cda393
Bhupendra <bhupendra.kushwah@agamitechnologies.in>
2025-06-09 11:34:55 +0530

    implemtn  server

--- frontend/src/component/commonComponent/customFields/index.js
+++
@@ -1,9 +0,0 @@
-import Button from "./Button";
-import CustomMultiSelectField from "./CustomMultiSelectField";
-import CustomSearchField from "./CustomSearchField";
-import Datepicker from "./Datepicker";
-import Input from "./Input";
-import Toggle from "./Toggle";
-
-
-export {Button,CustomMultiSelectField,CustomSearchField,Datepicker,Input,Toggle}
